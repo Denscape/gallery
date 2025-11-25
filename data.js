31d69b33d943bb67d0c2ed3c6a646ee1df16424d
Denscape <dnmrrdnd@gmail.com>
2025-11-25 20:07:25 +0800

    Add project files, including images, styles, and scripts for Black Clover gallery

---
+++ data.js
@@ -0,0 +1,82 @@
+var classes = [
+    {
+        name: "Asta",
+        image:
+            "assets/astaPortrait.jpg",
+        Squad: "Black Bulls",
+        description:
+            "Determined, energetic orphan who aims to become the Wizard King despite having no magic.",
+    },
+    {
+        name: "Yuno",
+        image:
+            "assets/yunoPortrait.jpg",
+        Squad: "Golden Dawn",
+        description:
+            "Asta's childhood friend and rival; a calm, prodigiously talented mage with wind-based magic.",
+    },
+    {
+        name: "Noelle Silva",
+        image:
+            "assets/noellePortrait.jpg",
+        Squad: "Black Bulls",
+        description:
+            "A noblewoman with powerful water magic who grows confident as a Black Bulls member.",
+    },
+    {
+        name: "Luck Voltia",
+        image:
+            "assets/luckPortrait.jpg",
+        Squad: "Black Bulls",
+        description:
+            "A battle-obsessed fighter with lightning-based magic and an upbeat, excitable demeanor.",
+    },
+    {
+        name: "Yami Sukehiro",
+        image:
+            "assets/yamiPortrait.jpg",
+        Squad: "Black Bulls",
+        description:
+            "The gruff Black Bulls captain who uses dark/anti-magic and pushes his squad to exceed their limits.",
+    },
+    {
+        name: "Zora Ideale",
+        image:
+            "assets/zoraPortrait.jpg",
+        Squad: "Black Bulls",
+        description:
+            "A pragmatic and rebellious Black Bulls member who specializes in trap-based magic and tactics.",
+    },
+    {
+        name: "Magna Swing",
+        image:
+            "assets/magnaPortrait.jpg",
+        Squad: "Black Bulls",
+        description:
+            "A hot-headed fighter who wields fire magic and fights with bold passion.",
+    },
+    {
+        name: "Charmy Pappitson",
+        image:
+            "assets/charmyPortrait.jpg",
+        Squad: "Black Bulls",
+        description:
+            "A food-loving Black Bulls member whose cotton-based magic hides impressive power.",
+    },
+    {
+        name: "Leopold Vermillion",
+        image:
+            "assets/leopoldPortrait.jpg",
+        Squad: "Golden Dawn",
+        description:
+            "A Golden Dawn member with noble bearing and strong fire-based magic.",
+    },
+    {
+        name: "Julius Novachrono",
+        image:
+            "assets/juliusPortrait.jpg",
+        Squad: "Aqua Deer",
+        description:
+            "The Wizard King famed for his time-manipulation magic and inquisitive, compassionate leadership.",
+    }
+];
